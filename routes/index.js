26cd1d88478d41fbba84d03583ff8b07bde0079d
Gurpanth <gurpanth1996@gmail.com>
2015-10-07 20:59:22 -0500

    Changes5

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,31 @@
 
-/**var express = require('express');
-var router = express.Router();**/
-
-
-/* GET home page. */
-/*
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;*/
-
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'First Express Generator Site',
-                        message: 'Our first page'} );
+  res.render('index', { title: 'This is the test website',
+                        message: 'My portfolio'} );
 });
 
+/*Get the main index file*/
 
+router.get('/', function(req, res, next) {
+   res.render('index'); 
+});
 /* GET ABOUT page */
 router.get('/about', function(req, res, next) {
    res.render('about'); 
 });
-
+/*GET PROJECTS page*/
 router.get('/projects', function(req, res, next) {
    res.render('projects'); 
 });
-
+/*GET SERVICES page*/
 router.get('/services', function(req, res, next) {
    res.render('services'); 
 });
-
+/*GET CONTACT page*/
 router.get('/contact', function(req, res, next) {
    res.render('contact'); 
 });
